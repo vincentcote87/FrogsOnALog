ffac83f2c6e9f78c29bd4425c18c14039baf7360
Vincent <vincentcote87@gmail.com>
2018-12-19 12:21:47 -0700

    Game runs with basic functionality

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const FROG = {
-  green: 'green',
-  red: 'red'
+  green: {color: 'green', img: 'imgs/frogGreen.png'},
+  red: {color: 'red', img: 'imgs/frogRed.png'}
 };
 
 const BLANK_SPACE = "blank";
 
-let posArray = [FROG.red, FROG.red, FROG.red, BLANK_SPACE, FROG.green, FROG.green, FROG.green];
+let posArray = [FROG.red.color, FROG.red.color, FROG.red.color, BLANK_SPACE, FROG.green.color, FROG.green.color, FROG.green.color];
 
 $('.frog').click(function () {
   let pos = $(this).index();
@@ -14,30 +14,42 @@ $('.frog').click(function () {
 
 function displayFrogs() {
   for (let i = 0; i < posArray.length; i++) {
-    $('#pos' + i).html(posArray[i]);
+    if (posArray[i] == FROG.red.color) {
+      $('#pos' + i).html('<img src="' + FROG.red.img + '" alt="red frog" >');
+    } else if (posArray[i] == FROG.green.color) {
+      $('#pos' + i).html('<img src="' + FROG.green.img + '" alt="green frog" >');
+    } else {
+      $('#pos' + i).html('<img src="imgs/blankSpace.png" alt="" >');
+    }
   }
 }
 
 function makeMove(index) {
   switch (posArray[index]) {
-    case FROG.red:
+    case FROG.red.color:
       if (posArray[index + 1] == BLANK_SPACE) {
-        posArray[index + 1] = FROG.red;
+        posArray[index + 1] = FROG.red.color;
         posArray[index] = BLANK_SPACE;
       } else if (posArray[index + 2] == BLANK_SPACE) {
-        posArray[index + 2] = FROG.red;
+        posArray[index + 2] = FROG.red.color;
         posArray[index] = BLANK_SPACE;
       }
       break;
-    case FROG.green:
+    case FROG.green.color:
       if (posArray[index - 1] == BLANK_SPACE) {
-        posArray[index - 1] = FROG.green;
+        posArray[index - 1] = FROG.green.color;
         posArray[index] = BLANK_SPACE;
       } else if (posArray[index - 2] == BLANK_SPACE) {
-        posArray[index - 2] = FROG.green;
+        posArray[index - 2] = FROG.green.color;
         posArray[index] = BLANK_SPACE;
       }
       break;
   };
   displayFrogs();
+  console.log(posArray);
 }
+
+// window.onload = function() {
+//   let ctx = document.getElementById('canvas').getContext('2d');
+//   // ctx.drawImage(document.getElementById('background'), 0, 0);
+// }
